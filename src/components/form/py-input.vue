fb9e8f259f810cc0f547805cf8950766be78080d
pengyi <danielpog@163.com>
2020-11-25 22:41:30 +0800

    py-input

---
+++ src/components/form/py-input.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <input :type='type' :value='value' @input='onInput'/>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            value: {
+                type: String,
+                default: ''
+            },
+            type: {
+                type: String,
+                default: 'text'
+            }
+        },
+        data() {
+            return {
+                
+            }
+        },
+        methods: {
+            onInput(e) {
+                this.$emit('input', e.target.value)
+            }
+        },
+    }
+</script>
---
+++ src/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <py-input v-model="userInfo.username"></py-input>
+        <p>{{userInfo.username}}</p>
+    </div>
+</template>
+
+<script>
+    import pyInput from '@/components/form/py-input'
+    export default {
+        data() {
+            return {
+                userInfo: {
+                    username: 'pengyi'
+                }
+            }
+        },
+        components: {
+            pyInput
+        },
+    }
+</script>
