6cebc5e314b205da87e1c4962cf6ea04e5cb145f
unknown <yi.peng@kingsunsoft.com>
2020-11-30 10:29:40 +0800

    通过Vue.extend()的方式注册组件

--- src/utils/dialog.js
+++ src/utils/dialog.js
@@ -4,25 +4,38 @@ import Vue from 'vue'
  * 自定义弹窗组件
  */
 const createDialog = function(Component, props){
-    // 1. render方式
-    const vm = new Vue({
-        // h 是createElement，返回vNode
+    // 2 Vue.extend() 方式
 
-        // 通过$mount挂载，得到真实dom
-        render: h => h(Component, { props })
+    const Ctor = Vue.extend(Component)
 
-    }).$mount() // 不指定宿主元素，需手动追加到body中，$mount不能指定body，指定body将会把body下所有元素都替换掉
+    const comp = new Ctor({ propsData: props})
+
+    comp.$mount() // 挂载
+
+    document.body.appendChild(comp.$el)
+
+    // // 1. render方式
+    // const vm = new Vue({
+    //     // h 是createElement，返回vNode
+
+    //     // 通过$mount挂载，得到真实dom
+    //     render: h => h(Component, { props })
+
+    // }).$mount() // 不指定宿主元素，需手动追加到body中，$mount不能指定body，指定body将会把body下所有元素都替换掉
 
     
 
-    document.body.appendChild(vm.$el)
+    // document.body.appendChild(vm.$el)
 
-    const comp = vm.$children[0] // 组件实例
+    // const comp = vm.$children[0] // 组件实例
+
+    
+
 
     // 销毁
     comp.remove = function() {
-        document.body.removeChild(vm.$el)
-        vm.$destroy()
+        document.body.removeChild(comp.$el)
+        comp.$destroy()
     }
 
     return comp
